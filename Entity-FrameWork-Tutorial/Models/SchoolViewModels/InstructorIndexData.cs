2471e94eb249cd0c77857b61070e077645759eb1
phạm anh tuấn <anhtuanst1@gmail.com>
2019-04-07 18:32:24 +0700

    Read related data

---
+++ Entity-FrameWork-Tutorial/Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entity_FrameWork_Tutorial.Models.SchoolViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
