bed6ab8f535634655c5d56355e2a8e1f8859dedb
phạm anh tuấn <anhtuanst1@gmail.com>
2019-04-07 16:28:33 +0700

    Sort, filter, page, and group

--- Entity-FrameWork-Tutorial/Controllers/HomeController.cs
+++ Entity-FrameWork-Tutorial/Controllers/HomeController.cs
@@ -5,11 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Entity_FrameWork_Tutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using Entity_FrameWork_Tutorial.Data;
+using Entity_FrameWork_Tutorial.Models.SchoolViewModels;
 
 namespace Entity_FrameWork_Tutorial.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
         public IActionResult Index()
         {
             return View();
